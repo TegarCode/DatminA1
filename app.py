706b5ba979db8379d0b3a10874a2b92debb33e77
TegarCode <105096476+TegarCode@users.noreply.github.com>
2024-12-19 02:09:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,5 +85,39 @@ def main():
             else:
                 st.error("Kolom 'full_text' tidak ditemukan dalam file yang diunggah.")
 
+
+    # Title untuk aplikasi
+    st.title("Prediksi dan Analisis Sentimen Tweet")
+
+    # Bagian untuk input tweet
+    tweet_input = st.text_area("Masukkan tweet di sini:")
+
+    if tweet_input:
+        # Load model dan vectorizer dari URL
+        model_url = "https://raw.githubusercontent.com/TegarCode/DatminA1/main/svm_sentiment_model.pkl"
+        vectorizer_url = "https://raw.githubusercontent.com/TegarCode/DatminA1/main/vectorizer.pkl"
+
+        model = load_model_from_url(model_url)
+        vectorizer = load_model_from_url(vectorizer_url)
+
+        # Pastikan model dan vectorizer berhasil di-load
+        if model and vectorizer:
+            # Transformasi data menggunakan vectorizer
+            tweet_vect = vectorizer.transform([tweet_input])
+
+            # Prediksi Sentimen
+            if st.button("Prediksi Sentimen"):
+                # Prediksi dengan model yang sudah dilatih
+                sentiment = model.predict(tweet_vect)
+
+                # Tampilkan hasil prediksi
+                if sentiment == 'positif':
+                    st.success(f"Sentimen tweet ini adalah: **Positif**")
+                elif sentiment == 'negatif':
+                    st.error(f"Sentimen tweet ini adalah: **Negatif**")
+                else:
+                    st.info(f"Sentimen tweet ini adalah: **Netral**")
+
+
 if __name__ == '__main__':
     main()
